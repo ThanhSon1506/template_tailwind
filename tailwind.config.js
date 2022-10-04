c11e3ffa0c91575b77d89f738fee7aafcd922b61
Son Thanh <coderthanhson@gmail.com>
2022-10-04 15:19:51 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px"
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      },
+      colors: {
+        'primary': '#06283D',
+        'secondary': '#256D85',
+        'tertiary': '#47B5FF',
+        'quaternary': '#DFF6FF'
+      }
+    },
+  },
+  plugins: [],
+}
